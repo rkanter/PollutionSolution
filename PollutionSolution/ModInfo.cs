371da2f951b0548d805ea5f7a8db9c93296f322c
Robert Kanter <robert.kanter@gmail.com>
2017-11-05 20:32:54 -0800

    some better organization

--- PollutionSolution/ModInfo.cs
+++ PollutionSolution/ModInfo.cs
@@ -9,7 +9,8 @@ namespace PollutionSolution
     // TODO: useful logging
     // TODO: ansel testing
     // TODO: license
-    public class PollutionSolution : ThreadingExtensionBase, IUserMod
+    // TODO: README
+    public class PollutionSolution : IUserMod
     {
         // It seems that if you make this too negative, it's less effective
         // (there's probably some underflow somewhere in that case)
@@ -18,12 +19,10 @@ namespace PollutionSolution
         // This didn't have the same problem as the noise, but it's safer to stay a bit away from the max int
         private const int pollutionAmount = Int32.MaxValue / 5;
 
-        private readonly Stopwatch timer = new Stopwatch();
-
-        // TODO: make this less ugly (i.e. set a better way instead of public static)
-        private bool removeNoisePollution = true;
-        private bool removeGroundPollution = true;
-        private bool removeWaterPollution = true;
+        // TODO: is there a better way to do this available to both classes than by making it static?
+        private static bool removeNoisePollution = true;
+        private static bool removeGroundPollution = true;
+        private static bool removeWaterPollution = true;
 
         public string Name
         {
@@ -44,44 +43,49 @@ namespace PollutionSolution
             settingsGroup.AddCheckbox("Water Pollution", true, (selected) => { removeWaterPollution = selected; });
         }
 
-        public override void OnCreated(IThreading threading)
+        public class PollutionLogic : ThreadingExtensionBase
         {
-            base.OnCreated(threading);
-            timer.Start();
-        }
+            private readonly Stopwatch timer = new Stopwatch();
 
-        public override void OnReleased()
-        {
-            timer.Reset();
-            base.OnReleased();
-        }
-        public override void OnAfterSimulationFrame()
-        {
-            // Probably not in a normal game type if the district manager doesn't actually exist.
-            if (Singleton<DistrictManager>.exists &&
-                // No need to do anything if we're paused
-                !SimulationManager.instance.SimulationPaused &&
-                // Don't do things faster than once per second
-                timer.ElapsedMilliseconds > 1000)
+            public override void OnCreated(IThreading threading)
             {
-                if (removeNoisePollution)
-                {
-                    ImmaterialResourceManager.instance.AddResource(ImmaterialResourceManager.Resource.NoisePollution, noiseAmount);
-                }
-                if (removeGroundPollution)
-                {
-                    Singleton<NaturalResourceManager>.instance.AddPollutionDisposeRate(pollutionAmount);
-                }
-                if (removeWaterPollution)
-                {
-                    Singleton<TerrainManager>.instance.WaterSimulation.AddPollutionDisposeRate(pollutionAmount);
-                }
-
-                timer.Reset();
+                base.OnCreated(threading);
                 timer.Start();
             }
 
-            base.OnAfterSimulationTick();
+            public override void OnReleased()
+            {
+                timer.Reset();
+                base.OnReleased();
+            }
+            public override void OnAfterSimulationFrame()
+            {
+                // Probably not in a normal game type if the district manager doesn't actually exist.
+                if (Singleton<DistrictManager>.exists &&
+                    // No need to do anything if we're paused
+                    !SimulationManager.instance.SimulationPaused &&
+                    // Don't do things faster than once per second
+                    timer.ElapsedMilliseconds > 1000)
+                {
+                    if (removeNoisePollution)
+                    {
+                        ImmaterialResourceManager.instance.AddResource(ImmaterialResourceManager.Resource.NoisePollution, noiseAmount);
+                    }
+                    if (removeGroundPollution)
+                    {
+                        Singleton<NaturalResourceManager>.instance.AddPollutionDisposeRate(pollutionAmount);
+                    }
+                    if (removeWaterPollution)
+                    {
+                        Singleton<TerrainManager>.instance.WaterSimulation.AddPollutionDisposeRate(pollutionAmount);
+                    }
+
+                    timer.Reset();
+                    timer.Start();
+                }
+
+                base.OnAfterSimulationTick();
+            }
         }
     }
 }
